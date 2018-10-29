007998bc2c18e53eac7e283220d2902043a5d2d0
Augustas <augustas@budnikas.lt>
2018-10-29 14:23:52 +0200

    fixes

--- M365/M365.ino
+++ M365/M365.ino
@@ -242,7 +242,7 @@ void displayFSM() {
   };
  
   m365_info.sph = (double)((double)S23CB0.speed / 1000.0);                 // speed
-  m365_info.spl = (double)((double)S23CB0.speed % 1000 / 100);
+  m365_info.spl = 0; // will be patched later
   m365_info.curh = abs(S25C31.current) / 100;       //current 
   //m365_info.curh = S25C31.current / 100;       //current //testing only
   m365_info.curl = abs(S25C31.current) % 100;
